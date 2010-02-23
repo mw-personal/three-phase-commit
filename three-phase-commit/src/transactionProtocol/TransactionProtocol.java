af323ca4c9d1b1e09868ccef0dcd2f8f14041671
jose.e.falcon <jose.e.falcon@83b4ca02-1d89-11df-baae-254496e1f253>
2010-02-23 23:44:43 +0000

    - updated transactionprotocol

--- three-phase-commit/src/transactionProtocol/TransactionProtocol.java
+++ three-phase-commit/src/transactionProtocol/TransactionProtocol.java
@@ -1,17 +1,6 @@
 package transactionProtocol;
 
-import java.net.InetSocketAddress;
-import java.util.List;
-
 public interface TransactionProtocol {
-	public void startTransaction();
-	
-	public List<InetSocketAddress> getParticiapnts();
-	public InetSocketAddress getCoordinator();
-	
-	
-	
-	
 	public Protocol getCommitProtocol();
 	public Protocol getTerminationProtocol();
 }
