d438fd4f4ab329638a8753f1e25c1c0c73791880
jose.e.falcon <jose.e.falcon@83b4ca02-1d89-11df-baae-254496e1f253>
2010-02-25 06:46:48 +0000

    - removing all the *junk*

--- three-phase-commit/src/transactionProtocol/Protocol.java
+++
@@ -1,5 +0,0 @@
-package transactionProtocol;
-
-public interface Protocol<R extends Request> {
-	public void start(Participant<R> p);
-}
--- three-phase-commit/src/transactionProtocol/TransactionProtocol.java
+++
@@ -1,6 +0,0 @@
-package transactionProtocol;
-
-public interface TransactionProtocol {
-	public Protocol getCommitProtocol();
-	public Protocol getTerminationProtocol();
-}
